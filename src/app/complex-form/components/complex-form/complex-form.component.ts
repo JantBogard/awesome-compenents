3efed2920f80f32663b1d56f8cf95108caeacfc3
JantBogard <joancenkonga@gmail.com>
2023-05-23 05:55:03 +0100

    Start to construct complex form

--- src/app/complex-form/components/complex-form/complex-form.component.ts
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -11,7 +11,13 @@ export class ComplexFormComponent implements OnInit {
   public mainForm!: FormGroup;
   public personnalInfoForm!: FormGroup;
   public contactPreferenceCtrl!: FormControl;
+  public emailCtrl!: FormControl;
+  public confirmEmailCtrl!: FormControl;
+  public emailForm!: FormGroup;
   public phoneCtrl!: FormControl;
+  public passwordCtrl!: FormControl;
+  public confirmPasswordCtrl!: FormControl;
+  public loginInfoForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -30,7 +36,20 @@ export class ComplexFormComponent implements OnInit {
       lastName: ['', Validators.required],
     });
     this.contactPreferenceCtrl = this.formBuilder.control('email');
+    this.emailCtrl = this.formBuilder.control('');
+    this.confirmEmailCtrl = this.formBuilder.control('');
+    this.emailForm = this.formBuilder.group({
+      email: this.emailCtrl,
+      confirm: this.confirmEmailCtrl
+    })
     this.phoneCtrl = this.formBuilder.control('');
+    this.passwordCtrl = this.formBuilder.control('', Validators.required);
+    this.confirmPasswordCtrl = this.formBuilder.control('', Validators.required);
+    this.loginInfoForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl
+    })
   }
 
   onSubmitForm(): void {
